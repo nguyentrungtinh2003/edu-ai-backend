51695c198e38e8baa563dfa51e8316a658473bb7
nguyentrungtinh <trungtinhn300@gmail.com>
2025-03-29 09:13:49 +0700

    Init project edu AI Agent

---
+++ src/test/java/com/TrungTinhBackend/edu_ai_backend/EduAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrungTinhBackend.edu_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
